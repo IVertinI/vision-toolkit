e5e56428a2bfbab7c2a5fe751efec20920d0330e
彭逸凡 <2440676130@qq.com>
2026-05-08 18:28:00 +0800

    feat:项目初始化

---
+++ build/CMakeFiles/test_filters.dir/tests/test_filters.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/test_filters.dir/tests/test_filters.cpp.o: \
+ /home/vertin/vision-toolkit/tests/test_filters.cpp \
+ /usr/include/stdc-predef.h
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vertin/vision-toolkit
+# Build directory: /home/vertin/vision-toolkit/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(FilterTests "/home/vertin/vision-toolkit/build/test_filters")
+set_tests_properties(FilterTests PROPERTIES  _BACKTRACE_TRIPLES "/home/vertin/vision-toolkit/CMakeLists.txt;28;add_test;/home/vertin/vision-toolkit/CMakeLists.txt;0;")
