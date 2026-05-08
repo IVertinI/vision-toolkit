c2f5bd6f8b34f07763241cd99c9396c5f533d0fb
彭逸凡 <2440676130@qq.com>
2026-05-08 23:34:29 +0800

    feat: add vision toolkit by pyf彭逸凡

---
+++ submissions/pyf/build/CMakeFiles/vision_toolkit.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vision_toolkit.dir/src/filters.cpp.o"
+  "CMakeFiles/vision_toolkit.dir/src/filters.cpp.o.d"
+  "CMakeFiles/vision_toolkit.dir/src/transforms.cpp.o"
+  "CMakeFiles/vision_toolkit.dir/src/transforms.cpp.o.d"
+  "libvision_toolkit.a"
+  "libvision_toolkit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vision_toolkit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
